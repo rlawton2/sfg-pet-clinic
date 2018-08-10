e317aea03a118abe2d6a54bf32f35dd3b60fa9b4
rlawton2 <rlawton1234@gmail.com>
2018-08-09 20:37:41 -0700

    initial commit

---
+++ src/main/java/com/lawton/ron/srfpetclinic/SrfPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.lawton.ron.srfpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrfPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrfPetClinicApplication.class, args);
+    }
+}
