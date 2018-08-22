8b7d87d52a659b24a40d92a49905cb0042b1fa90
rlawton2 <rlawton1234@gmail.com>
2018-08-21 22:34:21 -0700

    Refactor to common crud interface. Closes #11.

--- pet-clinic-data/src/main/java/com/lawton/ron/sfgpetclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/com/lawton/ron/sfgpetclinic/service/VetService.java
@@ -2,13 +2,6 @@ package com.lawton.ron.sfgpetclinic.service;
 
 import com.lawton.ron.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
